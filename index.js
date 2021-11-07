5b472ee56ea0e3e6c975967e183a68ad4c6f2aea
Graham <graham.lim88@gmail.com>
2021-11-07 15:52:39 +0800

    more work done

--- index.js
+++ index.js
@@ -11,29 +11,65 @@ app.set('view engine', 'ejs');
 
 /* We bind request-processing middleware like express.urlencoded to Express before our routes. This configures Express to process requests using that middleware before we handle those requests in our routes */
 app.use(express.urlencoded({ extended: false }));
+// we need this line to tell EJS where to look for static assets like gifs and imgs
+app.use(express.static('public'));
 
 const handleSightingIndexRequest = (request, response) => {
   read('data.json', (err, data) => {
-    const content = `
-      <html>
-        <body>
-          <h1> UFO Sighting </h1>
-            <p>Text: ${(data.sightings[request.params.index].text)} <br><br>
-            Date and Time: ${(data.sightings[request.params.index].date_time)} <br><br>
-            City: <br> ${(data.sightings[request.params.index].city)}<br><br>
-            State: <br> ${(data.sightings[request.params.index].state)}<br><br>
-            Shape: <br> ${(data.sightings[request.params.index].shape)}<br><br>
-            Duration: <br> ${(data.sightings[request.params.index].duration)}<br><br>
-            Summary: <br> ${(data.sightings[request.params.index].summary)}<br><br>
-            <a href="/">Back to Main Page</a>
-
-            </p>
-        </body>
-      </html>
-    `;
-    response.send(content);
+    const indexNumber = request.params.index;
+    const content = {
+      sightingIndex: {
+        index: indexNumber,
+        info: data.sightings[indexNumber],
+      },
+    };
+    response.render('sightingIndex', content);
   });
 };
+
+function handleAllSightings(request, response) {
+  // Obtain data to inject into EJS template
+  // data must be in the form of an object
+  read('data.json', (err, data) => {
+    const content = { allSightings: data.sightings };
+    console.log(`length of sightings data is ${data.sightings.length}`);
+    response.render('allSightings', content);
+  });
+}
+
+// Render the form to input new recipes
+app.get('/sighting', (request, response) => {
+  response.render('sightingForm');
+});
+
+app.post('/sighting', (request, response) => {
+  // Add new recipe data in request.body to recipes array in data.json.
+
+  add('data.json', 'sightings', request.body, (err) => {
+    if (err) {
+      response.status(500).send('DB write error.');
+      return;
+    }
+    // Acknowledge recipe saved.
+    // response.send('Saved sighting!');
+
+    read('data.json', (err, data) => {
+      console.log(`length of sightings data is ${data.sightings.length}`);
+      const newIndex = data.sightings.length - 1;
+      response.redirect(`/sighting/${newIndex}`);
+    });
+  });
+});
+
+app.get('/', handleAllSightings);
+
+app.get('/sighting/:index', handleSightingIndexRequest);
+
+// app.get('/year-sightings', handleYearIndex);
+
+// app.get('/year-sightings/:year', handleYearRequest);
+
+app.listen(3006);
 
 // const handleYearRequest = (request, response) => {
 //   read('data.json', (err, data) => {
@@ -83,45 +119,3 @@ const handleSightingIndexRequest = (request, response) => {
 //     // response.send(yearArray);
 //   });
 // }
-
-function handleAllSightings(request, response) {
-  // Obtain data to inject into EJS template
-  // data must be in the form of an object
-  read('data.json', (err, data) => {
-    const content = { allSightings: data.sightings };
-    console.log(`length of sightings data is ${data.sightings.length}`);
-    response.render('allSightings', content);
-  });
-}
-
-// Render the form to input new recipes
-app.get('/sighting', handleAllSightings);
-
-app.post('/sighting', (request, response) => {
-  // Add new recipe data in request.body to recipes array in data.json.
-
-  add('data.json', 'sightings', request.body, (err) => {
-    if (err) {
-      response.status(500).send('DB write error.');
-      return;
-    }
-    // Acknowledge recipe saved.
-    // response.send('Saved sighting!');
-
-    read('data.json', (err, data) => {
-      console.log(`length of sightings data is ${data.sightings.length}`);
-      const newIndex = data.sightings.length - 1;
-      response.redirect(`/sighting/${newIndex}`);
-    });
-  });
-});
-
-app.get('/', handleAllSightings);
-
-app.get('/sighting/:index', handleSightingIndexRequest);
-
-// app.get('/year-sightings', handleYearIndex);
-
-// app.get('/year-sightings/:year', handleYearRequest);
-
-app.listen(3004);
